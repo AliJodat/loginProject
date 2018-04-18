010d7a0f6f777b9f6da592ff0bc498faf9d7bbbb
Ali <Ali.Jodat3d@Gmail.com>
2018-04-18 23:20:55 +0430

    First commit

---
+++ src/main/java/org/logginProject/model/GroupPerson.java
@@ -0,0 +1,36 @@
+package org.logginProject.model;
+
+import javax.persistence.*;
+
+/**
+ * @author Ali
+ * Create on 4/18/2018 10:51 PM
+ */
+
+@Entity
+public class GroupPerson {
+
+    @Id
+    @SequenceGenerator(sequenceName = "Group_Person_Seq", name = "groupPersonSeq", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.IDENTITY, generator = "groupPersonSeq")
+    private Integer id;
+
+    @Column
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/logginProject/model/Personal.java
@@ -0,0 +1,77 @@
+package org.logginProject.model;
+
+import javax.persistence.*;
+
+/**
+ * @author Ali
+ * Create on 4/18/2018 10:49 PM
+ */
+
+@Entity
+public class Personal {
+
+    @Id
+    @SequenceGenerator(sequenceName = "Group_Person_Seq", name = "groupPersonSeq", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.IDENTITY, generator = "groupPersonSeq")
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String family;
+    @Column
+    private String username;
+    @Column
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private GroupPerson GroupPerson;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public org.logginProject.model.GroupPerson getGroupPerson() {
+        return GroupPerson;
+    }
+
+    public void setGroupPerson(org.logginProject.model.GroupPerson groupPerson) {
+        GroupPerson = groupPerson;
+    }
+}
