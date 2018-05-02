ecff5b63084758a3ba3d7bcbffb25537b6f1babc
user <mohsennoghly@gmail.com>
2018-05-02 16:00:27 +0430

    insert group personal2

--- src/main/java/org/loginProject/service/IPersonalService.java
+++ src/main/java/org/loginProject/service/IPersonalService.java
@@ -2,7 +2,7 @@ package org.loginProject.service;
 import org.loginProject.model.Personal;
 import org.loginProject.service.generic.IGenericService;
 import org.springframework.transaction.annotation.Transactional;
-public interface IPersonalService extends IGenericService<Personal> {
+public interface IPersonalService extends  IGenericService<Personal> {
     void save (Personal entity);
     void findPersonalByUsername (Personal entity);
 }
